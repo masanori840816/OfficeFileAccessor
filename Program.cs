37a6f772503faed0ce1102414479900bbd047920
Masui <masanori840816@gmail.com>
2024-11-20 07:37:40 +0900

    add packages

--- Program.cs
+++ Program.cs
@@ -1,6 +1,43 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using System.Text.Json.Serialization;
+using NLog;
+using NLog.Web;
 
-app.MapGet("/", () => "Hello World!");
+var logger = LogManager.Setup().LoadConfigurationFromFile("Nlog.config").GetCurrentClassLogger();
 
-app.Run();
+try
+{
+    var builder = WebApplication.CreateBuilder(args);
+    builder.Logging.ClearProviders();
+    builder.Logging.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace);
+    builder.Host.UseNLog();
+    
+    builder.Services.AddAntiforgery();
+    builder.Services.AddSession(options =>
+    {
+        options.IdleTimeout = TimeSpan.FromSeconds(30);
+        options.Cookie.HttpOnly = true;
+        options.Cookie.IsEssential = true;
+        options.Cookie.SameSite = SameSiteMode.Strict;
+    });
+    builder.Services.AddControllers()
+        .AddJsonOptions(options =>
+        {
+            // ReferenceLoopを切る.
+            options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+        });
+    var app = builder.Build();
+    
+    app.UseRouting();
+    app.UseSession();
+    app.MapStaticAssets();
+    app.MapControllers();
+    app.Run();
+}
+catch (Exception ex) when (ex is not HostAbortedException)
+{
+    logger.Fatal(ex, "Host terminated unexpectedly.");
+}
+finally
+{
+    LogManager.Shutdown();
+}
