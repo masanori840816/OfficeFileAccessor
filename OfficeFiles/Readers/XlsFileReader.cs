05a6a4d064a50ab374e50f1dc449f2ce980d252e
Masui Masanori <Masui.Masanori@ma.mee.co.jp>
2024-12-18 18:24:53 +0900

    Read a spreadsheet and getting Workbook and Worksheets

--- OfficeFiles/Readers/XlsFileReader.cs
+++ OfficeFiles/Readers/XlsFileReader.cs
@@ -1,3 +1,6 @@
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+
 namespace OfficeFileAccessor.OfficeFiles.Readers;
 
 public class XlsFileReader: IOfficeFileReader
@@ -10,5 +13,45 @@ public class XlsFileReader: IOfficeFileReader
     public void Read(IFormFile file)
     {
         logger.Info("Read Xls");
+        using SpreadsheetDocument spreadsheet = SpreadsheetDocument.Open(file.OpenReadStream(), false);
+        WorkbookPart? bookPart = spreadsheet.WorkbookPart;
+        if(bookPart == null)
+        {
+            logger.Info("Failed getting WorkbookPart");
+            return;
+        }
+        List<string> sheetNames = GetSheetNameList(bookPart);
+        foreach(var name in sheetNames)
+        {
+            logger.Info($"SheetName: {name}");
+            Worksheet? targetSheet = GetWorkSheet(bookPart, name);
+            if(targetSheet == null)
+            {
+                logger.Info($"Failed getting Worksheet Name: {name}");
+                return;
+            }
+        }
+       
+        logger.Info("OK");
+    }
+    private static List<string> GetSheetNameList(WorkbookPart bookPart) => 
+        bookPart.Workbook.Descendants<Sheet>().Where(s => string.IsNullOrEmpty(s.Name) == false)
+            .Select(s => s.Name?.Value ?? "").ToList();
+    private static Worksheet? GetWorkSheet(WorkbookPart bookPart, string sheetName)
+    {
+        foreach(Sheet s in bookPart.Workbook.Descendants<Sheet>())
+        {
+            if(s.Name == sheetName && string.IsNullOrEmpty(s.Id) == false)
+            {
+                if(bookPart.TryGetPartById(s.Id!, out var part))
+                {
+                    if (part is WorksheetPart result)
+                    {
+                        return result.Worksheet;
+                    }
+                }
+            }
+        }
+        return null;
     }
 }
