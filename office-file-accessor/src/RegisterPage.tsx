dcdd7b55e487bebc94a59b1dfcf1e7c11f22a6b2
Masui <masanori840816@gmail.com>
2025-01-23 00:57:39 +0900

    set XSRF token on uploading files

--- office-file-accessor/src/RegisterPage.tsx
+++ office-file-accessor/src/RegisterPage.tsx
@@ -1,30 +1,53 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { getServerUrl } from "./web/serverUrlGetter";
+import { getCookieValue } from "./web/cookieValues";
+import { useAuthentication } from "./auth/authenticationContext";
+import * as authStatusChecker from "./auth/authenticationStatusChecker";
+import { hasAnyTexts } from "./texts/hasAnyTexts";
 
 export function RegisterPage(): JSX.Element {
-    const [files, setFiles] = useState<FileList|null>(null);
-
+  const [files, setFiles] = useState<FileList|null>(null);
+  const authContext = useAuthentication();
+  useEffect(() => {
+          authStatusChecker.checkStatus(authContext);
+      }, [authContext]);
   const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setFiles(event.target.files);
   };
 
-  const handleUpload = () => {
+  const handleUpload = async () => {
     if (!files) {
       alert("Please select files first!");
       return;
     }
-
-    const formData = new FormData();
-    Array.from(files).forEach((file) => {
-      formData.append("files", file);
-    });
-    fetch(`${getServerUrl()}/api/files`, {
+    if(authContext == null) {
+      console.error("No auth context");
+      return;
+    }
+    try {
+      // check sign-in and get XSRF-Token
+      await authStatusChecker.checkStatus(authContext);
+      const cookieValue = getCookieValue("XSRF-TOKEN");
+      if(!hasAnyTexts(cookieValue)) {
+          throw Error("Invalid token");
+      }
+      const formData = new FormData();
+      Array.from(files).forEach((file) => {
+        formData.append("files", file);
+      });
+      const res = await fetch(`${getServerUrl()}/api/files`, {
+        mode: "cors",
         method: "POST",
+        headers: {
+          "X-XSRF-TOKEN": cookieValue,
+        },
         body: formData,
-      })
-      .then(res => res.text())
-      .then(res => console.log(res))
-      .catch(err => console.error("Upload failed", err));
+      });
+      console.log(await res.text());
+
+    } catch(err) {
+      console.error("Upload failed", err);
+    }
   };
 
   return (
