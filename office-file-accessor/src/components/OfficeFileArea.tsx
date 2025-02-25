a91fb9880bde51f457199c5f8a62d73973df447d
Masui Masanori <Masui.Masanori@ma.mee.co.jp>
2025-02-25 18:22:15 +0900

    [need implementation] style group area

--- office-file-accessor/src/components/OfficeFileArea.tsx
+++ office-file-accessor/src/components/OfficeFileArea.tsx
@@ -8,7 +8,7 @@ export interface OfficeFileAreaProps {
 }
 export const OfficeFileArea: React.FC<OfficeFileAreaProps> = ({file, dpi}) => {
     
-    return <div className="p-[2%] border border-solid rounded-lg border-[black]">
+    return <div className="p-[1vw] border border-solid rounded-lg border-[black]">
         <div className="pb-3">
             <h3>File: <label className="font-bold">{file.fileName}</label></h3>
         </div>
--- office-file-accessor/src/components/TableGroupArea.tsx
+++ office-file-accessor/src/components/TableGroupArea.tsx
@@ -1,6 +1,9 @@
-import { OfficeFileTableColumnWidth, OfficeFileTableGroup, OfficeFileTableRowHeight } from "../officeFileAccessor.type";
-import { hasAnyTexts } from "../texts/hasAnyTexts";
-import { TableCell } from "./TableCell";
+import { useEffect, useRef } from 'react';
+import { OfficeFileTableColumnWidth, OfficeFileTableGroup, OfficeFileTableRowHeight } from '../officeFileAccessor.type';
+import { hasAnyTexts } from '../texts/hasAnyTexts';
+import { TableCell } from './TableCell';
+import * as pixels from '../numbers/pixelConverter';
+
 
 export interface TableGroupAreaProps {
     group: OfficeFileTableGroup,
@@ -8,16 +11,32 @@ export interface TableGroupAreaProps {
     widths: OfficeFileTableColumnWidth[],
     heights: OfficeFileTableRowHeight[],
 }
-export const TableGroupArea: React.FC<TableGroupAreaProps> = ({group, dpi}) => {
+export const TableGroupArea: React.FC<TableGroupAreaProps> = ({group, dpi, widths, heights}) => {
+    const gridRef = useRef<HTMLDivElement>(null);
+    useEffect(() => {
+
+        if(gridRef.current == null) {
+            return;
+        }
+        let gridColumns = '';
+        for(const w of widths){
+            gridColumns += `${pixels.convertCentimeterToPixel((w.width * 1.2), dpi)}px `;
+        }
+        gridRef.current.style.gridTemplateColumns = gridColumns;
+
+    }, [widths, dpi]);
+    console.log(heights);
     return <>
         {hasAnyTexts(group.title)? (
-            <div>{group.title}</div>
+            <h4 className="pb-3">{group.title}</h4>
         ): <span></span>}
         
-        <div className="flex flex-row items-center flex-wrap w-full">
+        <div ref={gridRef} className='grid'>
+            <div className='border row-span-4'></div>
             {group.cells.map((c, index) => (
                 <TableCell key={index} cell={c} dpi={dpi} />
             ))}
+            
         </div>
     </>
 };
