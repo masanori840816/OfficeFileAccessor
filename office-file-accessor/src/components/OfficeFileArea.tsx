1c7f21a461d9c455175005e56251cb3d8fd4d154
Masui Masanori <Masui.Masanori@ma.mee.co.jp>
2025-02-25 13:30:30 +0900

    style showing file-area

--- office-file-accessor/src/components/OfficeFileArea.tsx
+++ office-file-accessor/src/components/OfficeFileArea.tsx
@@ -8,12 +8,14 @@ export interface OfficeFileAreaProps {
 }
 export const OfficeFileArea: React.FC<OfficeFileAreaProps> = ({file, dpi}) => {
     
-    return <>
-        <h2>{file.fileName}</h2>
+    return <div className="p-[2%] border border-solid rounded-lg border-[black]">
+        <div className="pb-3">
+            <h3>File: <label className="font-bold">{file.fileName}</label></h3>
+        </div>
         {file.sheets.map((s, index) => (
             <React.Fragment key={index}>
                 <div><OfficeFileSheetArea key={index} sheet={s} dpi={dpi} /></div>
             </React.Fragment>
         ))}
-    </>;
+    </div>;
 }
