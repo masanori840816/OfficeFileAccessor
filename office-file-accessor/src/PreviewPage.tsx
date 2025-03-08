3efdb72fabac241d2afbf88fe0d16cb2de709d1f
Masui Masanori <Masui.Masanori@ma.mee.co.jp>
2025-03-08 13:39:39 +0900

    update preview page dasign

--- office-file-accessor/src/PreviewPage.tsx
+++ office-file-accessor/src/PreviewPage.tsx
@@ -1,6 +1,7 @@
 
 import { useEffect, useState } from 'react';
 import { useAuthentication } from './auth/authenticationContext';
+import { getServerUrl } from './web/serverUrlGetter';
 import * as authStatusChecker from './auth/authenticationStatusChecker';
 import * as pixels from './numbers/pixelConverter';
 import { OfficeFileArea } from './components/OfficeFileArea';
@@ -18,13 +19,31 @@ export function PreviewPage(): JSX.Element {
     }, [authContext]);
     useEffect(() => {
         setOfficeFile(null);
+
+        console.log(`${getServerUrl()}/api/files/previewsheets`);
     }, []);
     return <>
-        <section>
+        <section className='flex flex-row items-center justify-between w-[98%] h-[12%] ml-[1%]'>
+            <div className='w-[82%] h-full flex flex-row items-center justify-between'>
+                <div className='flex flex-col justify-between items-start w-[50%] h-[60%]'>
+                    <h3>動作検証用ファイル名.xlsx</h3>
+                    <div className='flex flex-row w-[50%] justify-between items-center'>
+                        <div>
+                            User: 増井将則
+                        </div>
+                        <div>
+                            Update: 2025-03-08 12:22
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <button className='min-w-[80px]'>Download</button>
+            </div>
+
             
-            ファイル情報エリア<div>{dpi}</div>
         </section>
-        <section>
+        <section className='w-[98%] h-[67%] ml-[1%] bg-green-50'>
             {officeFile == null ? (
                     <div></div>
                   ):
@@ -32,6 +51,10 @@ export function PreviewPage(): JSX.Element {
                     <OfficeFileArea file={officeFile} dpi={dpi} />
                   )}
         </section>
-        <section>シート選択エリア</section>
+        <section className='flex flex-row w-[98%] h-[6%] ml-[1%] bg-red-50 items-start overflow-x-auto overflow-y-hidden'>
+            <button className='mr-[1vw]'>p12-14.作業要領書</button>
+            <button className='mr-[1vw]'>p23-17-16.計器、ﾊﾞｯｸｱｯﾌﾟ</button>
+            <button className='mr-[1vw]'>p23-17-16.計器、ﾊﾞｯｸｱｯﾌﾟ(2)</button>
+        </section>
     </>
 }
--- office-file-accessor/src/components/GlobalHeader.tsx
+++ office-file-accessor/src/components/GlobalHeader.tsx
@@ -9,29 +9,29 @@ export function GlobalHeader(): JSX.Element {
     const authContext = useAuthentication();
     return <>
         {authContext?.signedIn != null ? (
-            <header className='w-full h-[14%] min-h-[100px] shadow-xl'>
-            <div className='flex flex-row w-full justify-between h-[60%]'>
-                <div className='text-3xl p-[1%] font-bold'>OfficeFileAccessor</div>
-                <div className='flex flex-col items-center justify-around h-full pr-[1%]'>
-                <div>
-                    Hello {authContext.signedIn.userName}!
+            <header className='w-full h-[14%] min-h-[100px] shadow-xl z-10'>
+                <div className='flex flex-row w-full justify-between h-[60%]'>
+                    <div className='text-3xl p-[1%] font-bold'>OfficeFileAccessor</div>
+                    <div className='flex flex-col items-center justify-around h-full pr-[1%]'>
+                    <div>
+                        Hello {authContext.signedIn.userName}!
+                    </div>
+                    <div className='h-[50%]'>
+                        <SignOutButton />
+                    </div>
+                    </div>
                 </div>
-                <div className='h-[50%]'>
-                    <SignOutButton />
+                <div className='w-full h-[38%] flex flex-row items-center justify-center'>
+                    <Link to='/pages/register' className='h-full w-[18%]'>
+                        <div className='header-tab mr-[2%]'>File</div>
+                    </Link>
+                    <Link to='/pages/officefiles/preview/' className='h-full w-[18%]'>
+                        <div className='header-tab mr-[2%]'>Output</div>
+                    </Link>
+                    <Link to='/pages/user' className='h-full w-[18%]'>
+                        <div className='header-tab'>User</div>
+                    </Link>                
                 </div>
-                </div>
-            </div>
-            <div className='w-full h-[38%] flex flex-row items-center justify-center'>
-                <Link to='/pages/register' className='h-full w-[18%]'>
-                    <div className='header-tab mr-[2%]'>File</div>
-                </Link>
-                <Link to='/pages/officefiles/preview/' className='h-full w-[18%]'>
-                    <div className='header-tab mr-[2%]'>Output</div>
-                </Link>
-                <Link to='/pages/user' className='h-full w-[18%]'>
-                    <div className='header-tab'>User</div>
-                </Link>                
-            </div>
             </header>
         ):(
             <div></div>
