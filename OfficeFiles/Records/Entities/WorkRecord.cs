753394ad1983075982a6b1ec48c44e058d9d1658
Masui Masanori <Masui.Masanori@ma.mee.co.jp>
2025-03-21 21:05:40 +0900

    add record tables

---
+++ OfficeFiles/Records/Entities/WorkRecord.cs
@@ -0,0 +1,31 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+using OfficeFileAccessor.AppUsers.Entities;
+using OfficeFileAccessor.OfficeFiles.Entities;
+
+namespace OfficeFileAccessor.OfficeFiles.Records.Entities;
+
+[Table("work_record")]
+public class WorkRecord
+{
+    [Key]
+    [Column("id")]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public long Id { get; init; }
+    [Required]
+    [Column("office_file_id")]
+    public long OfficeFileId { get; init; }
+    [Column("working_user_id")]
+    public int? WorkingUserId { get; set; }
+    [Column("finish_working_date", TypeName = "timestamp with time zone")]
+    public DateTime? FinishWorkingDate { get; set; }
+    [Required]
+    [Column("last_update_date", TypeName = "timestamp with time zone")]
+    public DateTime LastUpdateDate { get; set; }
+    [JsonIgnore]
+    public ApplicationUser? ApplicationUser { get; set; }
+    [JsonIgnore]
+    public OfficeFile? OfficeFile { get; init; }
+    public List<InputRecord> InputRecords { get; init; } = [];
+}
